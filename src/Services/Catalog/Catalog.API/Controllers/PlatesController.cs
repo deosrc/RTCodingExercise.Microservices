ea55f3eb27fcdf80aa737b2151e92288e1158d7b
Deosrc <deosrc@g-de.co.uk>
2024-07-05 16:42:39 +0100

    Add controller add method and unit tests

--- src/Services/Catalog/Catalog.API/Controllers/PlatesController.cs
+++ src/Services/Catalog/Catalog.API/Controllers/PlatesController.cs
@@ -19,4 +19,26 @@ public class PlatesController : ControllerBase
     {
         return new OkObjectResult(await _platesRepository.GetPlatesAsync(cancellationToken));
     }
+
+    [HttpPost("")]
+    [ProducesResponseType(typeof(Plate), (int)HttpStatusCode.OK)]
+    [ProducesResponseType(typeof(ProblemDetails), (int)HttpStatusCode.BadRequest)]
+    [ProducesResponseType(typeof(ProblemDetails), (int)HttpStatusCode.InternalServerError)]
+    public async Task<IActionResult> Add(Plate plate, CancellationToken cancellationToken = default)
+    {
+        if (plate.Id != Guid.Empty)
+            return Problem(
+                title: "Id should not be provided",
+                detail: "Id will be auto-generated and should not be provided.",
+                statusCode: (int)HttpStatusCode.BadRequest);
+
+        var result = await _platesRepository.AddPlateAsync(plate, cancellationToken);
+        if (!result.IsSuccess)
+            return Problem(
+                title: "Operation failed",
+                detail: result.Message,
+                statusCode: (int)HttpStatusCode.InternalServerError);
+
+        return Ok(result.Result);
+    }
 }
--- src/Services/Catalog/Catalog.UnitTests/Controllers/PlatesControllerTests.cs
+++ src/Services/Catalog/Catalog.UnitTests/Controllers/PlatesControllerTests.cs
@@ -4,6 +4,7 @@ using Catalog.API.Data.Repositories;
 using Catalog.Domain;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
+using System;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -37,5 +38,92 @@ public class PlatesControllerTests
         Assert.IsType<OkObjectResult>(result);
         var objectResult = (OkObjectResult)result;
         Assert.Equal(plates, objectResult.Value);
+    }
+
+    [Fact]
+    public async Task Add_WhenIdProvided_ReturnsProblem()
+    {
+        var plate = new Plate
+        {
+            Id = Guid.NewGuid()
+        };
+
+        // Act
+        var result = await _sut.Add(plate);
+
+        // Assert
+        Assert.IsType<ObjectResult>(result);
+
+        var objectResult = (ObjectResult)result;
+        Assert.Equal(400, objectResult.StatusCode);
+        Assert.NotNull(objectResult.Value);
+        Assert.IsType<ProblemDetails>(objectResult.Value);
+
+        var problemDetails = (ProblemDetails)objectResult.Value!;
+        Assert.Equal("Id should not be provided", problemDetails.Title);
+        Assert.Equal("Id will be auto-generated and should not be provided.", problemDetails.Detail);
+        Assert.Equal(400, problemDetails.Status);
+    }
+
+    [Fact]
+    public async Task Add_WhenAddFails_ReturnsProblem()
+    {
+        var plate = _fixture.Build<Plate>().With(x => x.Id, Guid.Empty).Create();
+
+        _mockPlateRepository
+            .Setup(x => x.AddPlateAsync(It.IsAny<Plate>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new OperationResult<Plate>
+            {
+                IsSuccess = false,
+                Message = "Test message"
+            });
+
+        var cancellationToken = new CancellationToken();
+
+        // Act
+        var result = await _sut.Add(plate, cancellationToken);
+
+        // Assert
+        _mockPlateRepository.Verify(x => x.AddPlateAsync(plate, cancellationToken));
+        Assert.IsType<ObjectResult>(result);
+
+        var objectResult = (ObjectResult)result;
+        Assert.Equal(500, objectResult.StatusCode);
+        Assert.NotNull(objectResult.Value);
+        Assert.IsType<ProblemDetails>(objectResult.Value);
+
+        var problemDetails = (ProblemDetails)objectResult.Value!;
+        Assert.Equal("Operation failed", problemDetails.Title);
+        Assert.Equal("Test message", problemDetails.Detail);
+        Assert.Equal(500, problemDetails.Status);
+    }
+
+    [Fact]
+    public async Task Add_WhenSuccessful_ReturnsPlate()
+    {
+        var plate = _fixture.Build<Plate>().With(x => x.Id, Guid.Empty).Create();
+
+        _mockPlateRepository
+            .Setup(x => x.AddPlateAsync(It.IsAny<Plate>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new OperationResult<Plate>
+            {
+                IsSuccess = true,
+                Message = "Test message",
+                Result = plate
+            });
+
+        var cancellationToken = new CancellationToken();
+
+        // Act
+        var result = await _sut.Add(plate, cancellationToken);
+
+        // Assert
+        _mockPlateRepository.Verify(x => x.AddPlateAsync(plate, cancellationToken));
+        Assert.IsType<OkObjectResult>(result);
+
+        var objectResult = (OkObjectResult)result;
+        Assert.Equal(200, objectResult.StatusCode);
+        Assert.NotNull(objectResult.Value);
+        Assert.Equal(plate, objectResult.Value);
     }
 }
