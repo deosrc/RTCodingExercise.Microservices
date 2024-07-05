bfe684c587b4bd577f7e4127f4e6e8c144efb853
Deosrc <deosrc@g-de.co.uk>
2024-07-05 13:23:36 +0100

    Add basic placeholder list page and remove unused pages

---
+++ src/Web/WebMVC/Controllers/PlatesController.cs
@@ -0,0 +1,19 @@
+﻿namespace RTCodingExercise.Microservices.Controllers;
+public class PlatesController : Controller
+{
+    public IActionResult Index()
+    {
+        var plates = new List<Plate>()
+        {
+            new()
+            {
+                Registration = "Test Reg",
+                PurchasePrice = 123.44M,
+                SalePrice = 123.33M,
+                Letters = "TEST",
+                Numbers = 123
+            }
+        };
+        return View(plates);
+    }
+}
