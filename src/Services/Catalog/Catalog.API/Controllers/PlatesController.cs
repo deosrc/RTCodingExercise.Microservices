103cbdb1843f450707999aac005e99f7068f5842
Deosrc <deosrc@g-de.co.uk>
2024-07-05 12:33:18 +0100

    Add test for listing plates

---
+++ src/Services/Catalog/Catalog.API/Controllers/PlatesController.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.API.Data.Repositories;
+
+namespace Catalog.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class PlatesController : ControllerBase
+{
+    private IPlateRepository _platesRepository;
+
+    public PlatesController(IPlateRepository platesRepository)
+    {
+        _platesRepository = platesRepository;
+    }
+
+    [HttpGet("")]
+    public Task<IActionResult> List(CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Services/Catalog/Catalog.UnitTests/Controllers/PlatesControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using AutoFixture;
+using Catalog.API.Controllers;
+using Catalog.API.Data.Repositories;
+using Catalog.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Catalog.UnitTests.Controllers;
+public class PlatesControllerTests
+{
+    private readonly Fixture _fixture = new();
+    private readonly Mock<IPlateRepository> _mockPlateRepository = new();
+    private readonly PlatesController _sut;
+
+    public PlatesControllerTests()
+    {
+        _sut = new(_mockPlateRepository.Object);
+    }
+
+    [Fact]
+    public async Task List_ReturnsAllPlates()
+    {
+        // Arrange
+        var plates = _fixture.CreateMany<Plate>(100);
+        _mockPlateRepository
+            .Setup(x => x.GetPlatesAsync(It.IsAny<CancellationToken>()))
+            .ReturnsAsync(plates);
+
+        // Act
+        var result = await _sut.List();
+
+        // Assert
+        Assert.IsType<OkObjectResult>(result);
+        var objectResult = (OkObjectResult)result;
+        Assert.Equal(plates, objectResult.Value);
+    }
+}
