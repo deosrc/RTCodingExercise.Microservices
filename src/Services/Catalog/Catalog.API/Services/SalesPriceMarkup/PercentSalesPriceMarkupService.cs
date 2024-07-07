86cd2ab54c004f622c86afb040415c87e7ccc52c
Deosrc <deosrc@g-de.co.uk>
2024-07-07 20:19:51 +0100

    Implement sales price markup

--- src/Services/Catalog/Catalog.API/Services/SalesPriceMarkup/PercentSalesPriceMarkupService.cs
+++ src/Services/Catalog/Catalog.API/Services/SalesPriceMarkup/PercentSalesPriceMarkupService.cs
@@ -1,9 +1,9 @@
-﻿
-namespace Catalog.API.Services.SalesPriceMarkup;
+﻿namespace Catalog.API.Services.SalesPriceMarkup;
 
 public class PercentSalesPriceMarkupService : ISalesPriceMarkupService
 {
     private readonly PercentSalesPriceMarkupOptions _options;
+    private readonly decimal _salesPriceMultiplier;
 
     public PercentSalesPriceMarkupService(IOptions<PercentSalesPriceMarkupOptions> options)
         : this(options.Value)
@@ -14,10 +14,14 @@ public class PercentSalesPriceMarkupService : ISalesPriceMarkupService
     public PercentSalesPriceMarkupService(PercentSalesPriceMarkupOptions options)
     {
         _options = options;
+
+        // Convert to multiplier. For example, 1.1 is 110% of original sale price.
+        _salesPriceMultiplier = (_options.PercentMarkup / 100.0M) + 1;
     }
 
     public void AddSalesPriceMarkup(Plate plate)
     {
-        throw new NotImplementedException();
+        plate.SalePrice *= _salesPriceMultiplier;
+        plate.SalesPriceMarkup = $"{_options.PercentMarkup}%";
     }
 }
