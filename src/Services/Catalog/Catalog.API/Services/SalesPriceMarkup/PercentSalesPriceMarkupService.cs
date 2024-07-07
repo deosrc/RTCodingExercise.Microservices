30769eb603ed952480d4d21fd328dcc9a71039f8
Deosrc <deosrc@g-de.co.uk>
2024-07-07 20:07:44 +0100

    Add unit tests and stub percent sales price markup service

---
+++ src/Services/Catalog/Catalog.API/Services/SalesPriceMarkup/PercentSalesPriceMarkupService.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Catalog.API.Services.SalesPriceMarkup;
+
+public class PercentSalesPriceMarkupService : ISalesPriceMarkupService
+{
+    private readonly PercentSalesPriceMarkupOptions _options;
+
+    public PercentSalesPriceMarkupService(IOptions<PercentSalesPriceMarkupOptions> options)
+        : this(options.Value)
+    {
+        // Nothing to do.
+    }
+
+    public PercentSalesPriceMarkupService(PercentSalesPriceMarkupOptions options)
+    {
+        _options = options;
+    }
+
+    public void AddSalesPriceMarkup(Plate plate)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Services/Catalog/Catalog.UnitTests/Services/SalesPriceMarkup/PercentSalesPriceMarkupServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using Catalog.API.Services.SalesPriceMarkup;
+using Catalog.Domain;
+using Xunit;
+
+namespace Catalog.UnitTests.Services.SalesPriceMarkup;
+public class PercentSalesPriceMarkupServiceTests
+{
+    [Theory]
+    [InlineData(5, 100, 105)]
+    [InlineData(5, 200, 210)]
+    [InlineData(5, 300, 315)]
+    [InlineData(10, 100, 110)]
+    [InlineData(20, 100, 120)]
+    public void AddSalesPriceMarkup(int percentMarkup, decimal salesPrice, decimal expected)
+    {
+        var plate = new Plate()
+        {
+            SalePrice = salesPrice
+        };
+
+        var sut = new PercentSalesPriceMarkupService(new PercentSalesPriceMarkupOptions()
+        {
+            PercentMarkup = percentMarkup,
+        });
+
+        sut.AddSalesPriceMarkup(plate);
+
+        Assert.Equal(expected, plate.SalePrice);
+        Assert.Equal($"{percentMarkup}%", plate.SalesPriceMarkup);
+    }
+}
