49be59cb9f48529d12e3886be1c8cbe1ab9614a8
Deosrc <deosrc@g-de.co.uk>
2024-07-11 14:10:25 +0100

    Add unit tests for paging options domain object

--- src/Services/Catalog/Catalog.Domain/PagingOptions.cs
+++ src/Services/Catalog/Catalog.Domain/PagingOptions.cs
@@ -3,9 +3,11 @@
 namespace Catalog.Domain;
 public record PagingOptions
 {
+    public const int DefaultItemsPerPage = 20;
+
     [Range(1, int.MaxValue)]
     public int Page { get; set; } = 1;
 
     [Range(10, 250)]
-    public int ItemsPerPage { get; set; } = 20;
+    public int ItemsPerPage { get; set; } = DefaultItemsPerPage;
 }
---
+++ src/Services/Catalog/Catalog.UnitTests/Domain/PagingOptionsTests.cs
@@ -0,0 +1,13 @@
+﻿using Catalog.Domain;
+using Xunit;
+
+namespace Catalog.UnitTests.Domain;
+public class PagingOptionsTests
+{
+    [Fact]
+    public void Constructor_SetsItemsPerPageDefault()
+    {
+        var pagingOptions = new PagingOptions();
+        Assert.Equal(PagingOptions.DefaultItemsPerPage, pagingOptions.ItemsPerPage);
+    }
+}
