f4311093948cba24300dab4dce7b6b7f5d6e3c47
Deosrc <deosrc@g-de.co.uk>
2024-07-14 17:48:48 +0100

    Implement promotion validity periods

--- src/Services/Promotions/Promotions.Api/Data/OptionsPromotionsRepository.cs
+++ src/Services/Promotions/Promotions.Api/Data/OptionsPromotionsRepository.cs
@@ -9,15 +9,34 @@ namespace Promotions.Api.Data;
 /// <remarks>
 /// Only suitable for small numbers of promotions.
 /// </remarks>
-public class OptionsPromotionsRepository(IOptionsMonitor<IEnumerable<Promotion>> options, TimeProvider timeProvider = null) : IPromotionsRepository
+public class OptionsPromotionsRepository(IOptionsMonitor<IEnumerable<Promotion>> options, ILogger<OptionsPromotionsRepository> logger, TimeProvider? timeProvider = null) : IPromotionsRepository
 {
     private readonly IOptionsMonitor<IEnumerable<Promotion>> _options = options;
+    private readonly ILogger<OptionsPromotionsRepository> _logger = logger;
     private readonly TimeProvider _timeProvider = timeProvider ?? TimeProvider.System;
 
     private IEnumerable<Promotion> Promotions => _options.CurrentValue;
 
     public Task<Promotion?> GetCurrentPromotionByCodeAsync(string code, CancellationToken cancellationToken = default)
     {
-        return Task.FromResult(Promotions.SingleOrDefault(x => x.Code.Equals(code, StringComparison.InvariantCultureIgnoreCase)));
+        var now = _timeProvider.GetUtcNow();
+
+        var promotions = Promotions.Where(x => x.Code.Equals(code, StringComparison.InvariantCultureIgnoreCase)).ToList();
+        _logger.LogInformation("Found {PromotionCount} promotions matching code {PromotionCode}.", promotions.Count, code);
+        if (promotions.Count == 0)
+            return Task.FromResult<Promotion?>(null);
+
+        var promotion = promotions
+            .Where(x => now > x.ValidFrom.ToUniversalTime())
+            .Where(x => now < x.ValidUntil.ToUniversalTime())
+            .SingleOrDefault();
+        if (promotion is null)
+        {
+            _logger.LogInformation("Promotion {PromotionCode} is not within the valid period.", code);
+            return Task.FromResult<Promotion?>(null);
+        }
+
+        _logger.LogInformation("Valid and current promotion found for {PromotionCode}.", code);
+        return Task.FromResult(Promotions.SingleOrDefault());
     }
 }
--- src/Services/Promotions/Promotions.UnitTests/Data/OptionsPromotionsRepositoryTests.cs
+++ src/Services/Promotions/Promotions.UnitTests/Data/OptionsPromotionsRepositoryTests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Options;
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Time.Testing;
 using Moq;
 using Promotions.Api.Data;
 using Promotions.Api.Data.Models;
@@ -6,6 +8,11 @@ using Promotions.Api.Data.Models;
 namespace Promotions.UnitTests.Data;
 public class OptionsPromotionsRepositoryTests
 {
+    /// <summary>
+    /// The current date and time according to the injected <see cref="TimeProvider"/>.
+    /// </summary>
+    private readonly DateTime MockedDateTime = new(2024, 07, 14, 17, 32, 01, DateTimeKind.Utc);
+
     private readonly List<Promotion> _promotions = [];
     private readonly OptionsPromotionsRepository _sut;
 
@@ -16,7 +23,7 @@ public class OptionsPromotionsRepositoryTests
             .SetupGet(x => x.CurrentValue)
             .Returns(() => _promotions);
 
-        _sut = new(optionsMonitor.Object);
+        _sut = new(optionsMonitor.Object, Mock.Of<ILogger<OptionsPromotionsRepository>>(), new FakeTimeProvider(MockedDateTime));
     }
 
     [Fact]
@@ -34,12 +41,36 @@ public class OptionsPromotionsRepositoryTests
     {
         var promotion = new Promotion
         {
-            Code = "TEST123"
+            Code = "TEST123",
+            ValidFrom = MockedDateTime.AddDays(-12),
+            ValidUntil = MockedDateTime.AddDays(12)
         };
         _promotions.Add(promotion);
 
         var result = await _sut.GetCurrentPromotionByCodeAsync(promotionCode);
         Assert.NotNull(result);
         Assert.Same(promotion, result);
+    }
+
+    /// <remarks>
+    /// Tests execute relative to <see cref="MockedDateTime"/>.
+    /// </remarks>
+    [Theory]
+    [InlineData("2024-07-01T17:32:01Z", "2024-07-12T17:32:00Z")] // Past
+    [InlineData("2024-07-01T17:32:01Z", "2024-07-14T17:32:00Z")] // Past (boundary)
+    [InlineData("2024-07-14T17:32:02Z", "2024-07-20T17:32:01Z")] // Future (boundary)
+    [InlineData("2024-07-18T17:32:02Z", "2024-07-20T17:32:01Z")] // Future
+    public async Task GetCurrentPromotionByCodeAsync_WhenOutsideValidPeriod_ReturnsNull(string validFrom, string validTo)
+    {
+        var promotion = new Promotion
+        {
+            Code = "TEST123",
+            ValidFrom = DateTime.Parse(validFrom),
+            ValidUntil = DateTime.Parse(validTo)
+        };
+        _promotions.Add(promotion);
+
+        var result = await _sut.GetCurrentPromotionByCodeAsync("TEST123");
+        Assert.Null(result);
     }
 }
