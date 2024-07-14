4eacbf373d242bee8b5f996cc77a15f6aaac0362
Deosrc <deosrc@g-de.co.uk>
2024-07-14 17:37:58 +0100

    Implement basic options based promotion repository

---
+++ src/Services/Promotions/Promotions.Api/Data/OptionsPromotionsRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Options;
+using Promotions.Api.Data.Models;
+
+namespace Promotions.Api.Data;
+
+/// <summary>
+/// Basic promotions repository reading data from options.
+/// </summary>
+/// <remarks>
+/// Only suitable for small numbers of promotions.
+/// </remarks>
+public class OptionsPromotionsRepository(IOptionsMonitor<IEnumerable<Promotion>> options, TimeProvider timeProvider = null) : IPromotionsRepository
+{
+    private readonly IOptionsMonitor<IEnumerable<Promotion>> _options = options;
+    private readonly TimeProvider _timeProvider = timeProvider ?? TimeProvider.System;
+
+    private IEnumerable<Promotion> Promotions => _options.CurrentValue;
+
+    public Task<Promotion?> GetCurrentPromotionByCodeAsync(string code, CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult(Promotions.SingleOrDefault(x => x.Code.Equals(code, StringComparison.InvariantCultureIgnoreCase)));
+    }
+}
---
+++ src/Services/Promotions/Promotions.UnitTests/Data/OptionsPromotionsRepositoryTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Options;
+using Moq;
+using Promotions.Api.Data;
+using Promotions.Api.Data.Models;
+
+namespace Promotions.UnitTests.Data;
+public class OptionsPromotionsRepositoryTests
+{
+    private readonly List<Promotion> _promotions = [];
+    private readonly OptionsPromotionsRepository _sut;
+
+    public OptionsPromotionsRepositoryTests()
+    {
+        var optionsMonitor = new Mock<IOptionsMonitor<IEnumerable<Promotion>>>();
+        optionsMonitor
+            .SetupGet(x => x.CurrentValue)
+            .Returns(() => _promotions);
+
+        _sut = new(optionsMonitor.Object);
+    }
+
+    [Fact]
+    public async Task GetCurrentPromotionByCodeAsync_WhenCodeNotAvailable_ReturnsNull()
+    {
+        var result = await _sut.GetCurrentPromotionByCodeAsync("TEST123");
+        Assert.Null(result);
+    }
+
+    [Theory]
+    [InlineData("TEST123")]
+    [InlineData("Test123")]
+    [InlineData("test123")]
+    public async Task GetCurrentPromotionByCodeAsync_WhenPromotionAvailable_ReturnsPromotion(string promotionCode)
+    {
+        var promotion = new Promotion
+        {
+            Code = "TEST123"
+        };
+        _promotions.Add(promotion);
+
+        var result = await _sut.GetCurrentPromotionByCodeAsync(promotionCode);
+        Assert.NotNull(result);
+        Assert.Same(promotion, result);
+    }
+}
